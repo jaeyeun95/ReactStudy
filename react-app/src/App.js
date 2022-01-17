c0c49c5784bf68122c45ad7c984a9902bbf66834
jaeyeun <jaeyeun95@gmail.com>
2022-01-17 23:50:31 +0900

    생활코딩 Component 만들기

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react/cjs/react.production.min";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+// header
+class Subject extends Component {
+  // class 안에 들어가 있는 함수는 function 선언이 없어도 된다.
+  render() {
+    // vue 에서 <template>에서처럼 최상위 태그 하나만 있어야 한다.
+    return (
+      <header>
+        <h1>WEB</h1>
+        world wide web!
       </header>
-    </div>
-  );
+    );
+  }
+}
+
+// nav
+class TOC extends Component {
+  render() {
+    return (
+      <nav>
+        <ul>
+          <li>
+            <a href="1.html">HTML</a>
+          </li>
+          <li>
+            <a href="2.html">CSS</a>
+          </li>
+          <li>
+            <a href="3.html">JavaScript</a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+// article
+class Content extends Component {
+  render() {
+    return (
+      <article>
+        <h2>HTML</h2>
+        HTML is HyperText Markup Language.
+      </article>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Subject />
+        <TOC />
+        <Content />
+      </div>
+    );
+  }
 }
 
 export default App;
