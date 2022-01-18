a9c874a36cff9996530a6d472e0581275192c589
jaeyeun <jaeyeun95@gmail.com>
2022-01-18 23:40:17 +0900

    addTodo 추가

--- todo-react-app/src/App.js
+++ todo-react-app/src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 import React from "react";
 import Todo from "./Todo";
-import { List, Paper } from "@material-ui/core";
+import AddTodo from "./AddTodo";
+import { Container, List, Paper } from "@material-ui/core";
 
 // class
 class App extends React.Component {
@@ -14,9 +15,17 @@ class App extends React.Component {
       ],
     };
   }
-  render() {
-    // 2. 자바스크립트 map 함수를 통해 배열 반복해서 <Todo .../> 컴포넌트 생성
 
+  // 1. 함수 추가
+  add = (item) => {
+    const thisItems = this.state.items;
+    item.id = "ID-" + thisItems.length; // key를 위한 id 추가
+    item.done = false; // done 초기화
+    thisItems.push(item);
+    this.setState({ item: thisItems }); // 업데이트는 반드시 this.setState로 해야됨
+    console.log("items : ", this.state.items);
+  };
+  render() {
     var todoItems = this.state.items.length > 0 && (
       <Paper style={{ margin: 16 }}>
         <List>
@@ -29,10 +38,10 @@ class App extends React.Component {
 
     // 3. 생성된 컴포넌트 리턴
     return (
-      <div className="App">
-        <h2>hi</h2>
-        {todoItems}
-      </div>
+      <Container maxWidth="md">
+        <AddTodo add={this.add} />
+        <div className="App">{todoItems}</div>
+      </Container>
     );
   }
 }
