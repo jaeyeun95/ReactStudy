9184b0bfef4dc2a22910de5a1b95cd81d6c564f5
jaeyeun <jaeyeun95@gmail.com>
2022-01-18 14:20:52 +0900

    state 사용법

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,12 +5,27 @@ import Content from "./components/Content";
 import Subject from "./components/Subject";
 
 class App extends Component {
+  // 컴포넌트가 실행될 때 render() 함수보다 먼저 실행되고 해당 컴포넌트를 초기화
+  constructor(props) {
+    super(props);
+    this.state = {
+      subject: { title: "WEB", sub: "World wide Web!" },
+      contents: [
+        { id: 1, title: "HTML", desc: "HTML is for infomation" },
+        { id: 2, title: "CSS", desc: "CSS is for design" },
+        { id: 3, title: "JavaScript", desc: "JavaScript is for interactive" },
+      ],
+    };
+  }
   render() {
     return (
       <div className="App">
-        <Subject title="WEB" sub="world wide web!" />
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+        />
         {/* <Subject title="React" sub="For UI" /> */}
-        <TOC />
+        <TOC data={this.state.contents} />
         <Content title="HTML" desc="HTML is HyperText Markup Language." />
       </div>
     );
